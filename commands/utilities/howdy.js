e2994c832a468505324729243986d8d6fb131047
Carlos Pinto <123394002+carlospinto8@users.noreply.github.com>
2025-02-21 19:28:48 -0500

    feat: [DONNADEV-2] implemented basic command handling

--- commands/utilities/howdy.js
+++ commands/utilities/howdy.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 /*
     NOTES:
-    1. New SlashCommands must have AT LEAST a name and a description 
+    1. New SlashCommands must have AT LEAST a name and a description
     2. Must be between 1-32 chars and cannot use capitals, spaces, or symbols (except - or _)
     3. Needs a function to execute when the command is called
 

d9b0ae17ef7d06d597c68bee22d24575f2beedbe
Carlos Pinto <123394002+carlospinto8@users.noreply.github.com>
2025-02-27 14:58:14 -0500

    feat: [DONNADEV-6] pokemon command will now let the user know which random pokemon they caught

--- commands/pokemon/pokemon.js
+++ commands/pokemon/pokemon.js
@@ -1,4 +1,9 @@
+const https = require("https");
 const { SlashCommandBuilder } = require("discord.js");
+
+function randomNumber() {
+  return Math.floor(Math.random() * (1025 - 1 + 1)) + 1;
+}
 
 module.exports = {
   cooldown: 20,
@@ -6,8 +11,29 @@ module.exports = {
     .setName("pokemon")
     .setDescription("This command lets you possibly catch a pokemon!"),
   async execute(interaction) {
-    await interaction.reply(
-      "Oop.. This command is blocked by a sleeping pokemon. Come back later.",
-    );
+    const randomPokemonId = randomNumber();
+
+    https
+      .get(`https://pokeapi.co/api/v2/pokemon/${randomPokemonId}`, (res) => {
+        let data = "";
+
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        res.on("end", async () => {
+          let caughtPokemon = JSON.parse(data);
+          console.log(caughtPokemon.name);
+          await interaction.reply(
+            `You threw a pokeball into the tall grass. Congratulations, you just caught ${caughtPokemon.name}!!`,
+          );
+        });
+      })
+      .on("error", async (err) => {
+        console.log("Error: " + err.message);
+        await interaction.reply(
+          "Oop.. This command is blocked by a sleeping pokemon. Come back later.",
+        );
+      });
   },
 };
